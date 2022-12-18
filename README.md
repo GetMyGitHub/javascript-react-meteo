953fe7b470a83ce25287924085a6159c356ac3ce
Christophe <35873784+GetMyGitHub@users.noreply.github.com>
2022-12-18 14:02:30 +0000

    calendrier

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # javascript-react-meteo
 meteo
+
+créé avec with npx create-react-app .
+
+plugin supplémentaires : 
+    - tailwindcss : https://tailwindcss.com/
+    - ES7+ React/Redux/React-Native snippets (dans vsCode)
---
+++ src/utils/formatWeatherDataDaily.js
@@ -0,0 +1,34 @@
+export const formatWeatherDataDaily = (data) => {
+    const dataDaily = [];
+    const dataEntries = Object.keys(data);
+    dataEntries.forEach((key, keyIndex) => {
+        for (let i = 0; i < data[key].length; i++) {
+            if (keyIndex === 0) {
+                dataDaily.push({});
+            }
+            const dayValue = data[key][i];
+            dataDaily[i][key] = dayValue;
+        }
+    });
+
+    dataDaily.forEach((data) => {
+        const date = new Date(data.time);
+        const dayIndex = date.getDay();
+        data.day = frenchDays[dayIndex];
+    });
+
+    return dataDaily;
+
+};
+
+
+
+const frenchDays = [
+    "Dimanche",
+    "Lundi",
+    "Mardi",
+    "Mercredi",
+    "Jeudi",
+    "Vendredi",
+    "Samedi",
+];
